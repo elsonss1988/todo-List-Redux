70e8f54f84c4555fd53cb8fc5d86b40f34e222f6
elsonss1988 <elsonss1988@gmail.com>
2020-10-10 10:44:55 -0300

    add componentes list,button,input

---
+++ src/componentes/list/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const List=({todoList})=>(
+    <ul>
+        {todoList.map(({id, name})=>(
+          <li key={id}>{name}</li>  
+        ))}
+    </ul>
+)
+export default List;
