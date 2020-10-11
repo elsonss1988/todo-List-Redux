8ee80ab140e69e194280063027cd99fd0dfed757
elsonss1988 <elsonss1988@gmail.com>
2020-10-11 05:07:27 -0300

    redux adicionado

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,11 +1,39 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import Button from '../../componentes/button';
+import List from '../../componentes/list';
+import Input from '../../componentes/input'; 
+
+import {addTodo} from '../../store/actions/todo'
 class App extends Component{
     state={
         input:''
     };
-    render(){
-        return <h1>Hey</h1>
-        }
-    }
 
-export default App;
+    handleOnClick=()=>{
+        console.log('Button was clicked');
+    };
+
+    handleOnChange=(event)=>{
+        //console.log('Input was clicked', event.target.value);
+        this.setState({input: event.target.value})
+        console.log(this.state);
+    }
+    render(){
+        const {input}=this.state;
+        const {listTodo}=this.props;
+        console.log(listTodo);
+        return (
+            <div>
+                <List todoList={[]}/>
+                <Input onChange={event=>this.handleOnChange(event)} value={input}/>
+                <Button onClick={()=>this.handleOnClick()}> Adicionar </Button>               
+            </div>
+        );
+    }
+}
+const mapStateToProps=state=>({
+    listTodo: state.todo
+});
+//export default App;
+export default connect(mapStateToProps, {addTodo})(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/app';
+import {Provider} from 'react-redux';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store';
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById('root')
+);
